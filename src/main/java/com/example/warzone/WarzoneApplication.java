7b825a8322858bf66c875f53639e2e9851b63dd7
AngryL1on <95445205+AngryL1on@users.noreply.github.com>
2024-02-27 12:38:07 +0300

    Move the ModelMapper bean to another file

--- src/main/java/com/example/warzone/WarzoneApplication.java
+++ src/main/java/com/example/warzone/WarzoneApplication.java
@@ -1,24 +1,10 @@
 package com.example.warzone;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WarzoneApplication {
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        modelMapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
-
-        return modelMapper;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(WarzoneApplication.class, args);
     }
